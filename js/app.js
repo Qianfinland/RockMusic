da13b1cabcf3c6be2b098d28a6f6880dcc91b8dc
Qianfinland <qianfinland@gmail.com>
2014-11-15 17:16:02 +0200

    app.js without router and route, list of colors disappear and welcome message avaliable

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
-});
+// App.Router.map(function() {
+//   // put your routes here
+// });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue'];
+//   }
+// });
